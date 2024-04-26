8388886aa901a1e7a1cf30f414974cc25544f242
Akshithach3 <akshitha8984@gmail.com>
2024-04-26 19:57:17 +0530

    project added

---
+++ Akshitha_Vue/src/components/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <navbar />
+
+  <main>
+    <div class="max-w-[1200px] mx-auto">
+      <router-view />
+    </div>
+  </main>
+  <BottomBar />
+</template>
+
+<script setup>
+import Navbar from "../components/Navbar.vue";
+import BottomBar from "./BottomBar.vue";
+
+</script>
